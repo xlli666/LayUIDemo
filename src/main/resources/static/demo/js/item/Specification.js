80cabacefe3d9a33dd9042743413a63dc1aac9c0
xlli666 <543011684@qq.com>
2019-01-04 17:51:49 +0800

    规格参数修改

--- src/main/resources/static/demo/js/item/Specification.js
+++ src/main/resources/static/demo/js/item/Specification.js
@@ -1,3 +1,22 @@
-layui.use('layer',function () {
+layui.use('form',function () {
+    var $ = layui.jquery;
+    var layer = layui.layer;
+    var form = layui.form;
 
+    form.on('submit(formSpecQuery)', function (data) {
+        layer.alert(JSON.stringify(data.field));
+        return false;
+    });
+    form.on('submit(formSpecAdd)', function (data) {
+        layer.alert(JSON.stringify(data.field));
+        return false;
+    });
+    form.on('submit(formSpecUpdate)', function (data) {
+        layer.alert(JSON.stringify(data.field));
+        return false;
+    });
+    form.on('submit(formSpecDelete)', function (data) {
+        layer.alert(JSON.stringify(data.field));
+        return false;
+    });
 });
