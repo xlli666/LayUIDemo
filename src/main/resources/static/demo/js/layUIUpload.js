85c678e8a9bc71f864978adf2cde61bdec7cb423
xlli666 <543011684@qq.com>
2019-03-05 14:56:37 +0800

    上传修改

--- src/main/resources/static/demo/js/layUIUpload.js
+++ src/main/resources/static/demo/js/layUIUpload.js
@@ -2,10 +2,12 @@ layui.use('upload', function(){
     var uploadVar = layui.upload;
     uploadVar.render({
         elem: '#uploadLayUI',
-        url: '/demo/upload',
-        //url: 'http://127.0.0.1:8082/upload/imageLayUI',
+        //url: '/demo/upload',
+        url: 'http://127.0.0.1:8082/upload/image',
         done: function (res) {
-            alert(res.data.src);
+            var rs = res.data.src;
+            alert(rs);
+            $('#uploadRs').attr('src', rs);
         },
         error: function (msg) {
             console.log(msg);
--- src/main/resources/static/demo/js/upload.js
+++ src/main/resources/static/demo/js/upload.js
@@ -2,23 +2,46 @@ $(function () {
     $('#uploadJquery').on('click',function () {
         uploadImg();
     });
+    $('#dltBtn').on('click',function () {
+        deleteImg();
+    });
 });
 function uploadImg() {
     var objData = new FormData($('#uploadForm')[0]);
     $.ajax({
         type : 'POST',
-        url : '/demo/upload',
-        //url: 'http://127.0.0.1:8082/upload/image',
+        //url : '/demo/upload',
+        url: 'http://127.0.0.1:8082/upload/image',
         data : objData,
         cache: false,
         processData: false,
         contentType: false,
         dataType : 'json',
         success: function (data) {
-            alert(data.data.src);
+            var rs = data.data.src;
+            alert(rs);
+            $('#uploadRs').attr('src', rs);
         },
         error : function (msg) {
             console.log('error: ' + msg);
+        }
+    });
+}
+
+function deleteImg() {
+    $.ajax({
+        type : 'POST',
+        //url : '/demo/upload',
+        url: 'http://127.0.0.1:8082/upload/delete',
+        data : {
+            fileUrl: 'http://192.168.43.141/group1/M00/00/00/wKgrjVx968CAF0dsAAAHR9A3qNg684_60x60.png'
+        },
+        dataType : 'json',
+        success: function (data) {
+            alert(data.data.src);
+        },
+        error : function (msg) {
+            alert(msg.status);
         }
     });
 }
